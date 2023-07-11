907ce4fa78310aefabfe88ad75a43db9002ccd31
taylorlottdixon <136010920+taylorlottdixon@users.noreply.github.com>
2023-07-11 14:21:58 -0500

    create header and footer partial ejs

--- controllers/todos.js
+++ controllers/todos.js
@@ -8,12 +8,14 @@ module.exports = {
 
 function index(req, res) {
     res.render('todos/index', {
-        todos: Todo.getAll()
+        todos: Todo.getAll(),
+        title: 'To-Do List',
     });
 }
 
 function show(req, res) {
     res.render('todos/show', {
       todo: Todo.getOne(req.params.id),
+      title: 'To-Do Details',
     });
   }
